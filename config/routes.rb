20669d00fc380130b8d898d52cad844fc1d286d7
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-09-11 12:18:02 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+  Rails.application.routes.draw do
+
+  root to: 'homes#top'
+  devise_for :users
+  get "/homes/about" => "homes#about", as: "about"
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
+end
