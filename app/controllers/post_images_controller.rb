a3d03e15a6c9c28785842b07f136f8eecc077c35
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-09-12 11:25:39 +0000

    完成

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -5,17 +5,21 @@ class PostImagesController < ApplicationController
 
   def create
     @post_image = PostImage.new(post_image_params)
-    @post_image.user_id = current_user.
-    @post_image.save
-    redirect_to post_images_path
+    @post_image.user_id = current_user.id
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   def destroy
